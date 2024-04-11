704462515693c8e4608e37dacc61a86ad447c1ff
dvauquelin <david.vauquelin@planisware.com>
2024-04-11 17:44:20 +0200

    Added Token contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -2,7 +2,13 @@
 pragma solidity 0.8.24;
 
 import "hardhat/console.sol";
+import "./Token.sol";
 
 contract Crowdsale {
     string public name = "Crowdsale";
+    Token public token;
+
+    constructor(Token _token) {
+        token = _token;
+    }
 }
--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -7,16 +7,27 @@ describe('Crowdsale contract', function () {
   async function deployCrowdsaleFixture() {
     const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const crowdsale = await ethers.deployContract('Crowdsale');
+    const Token = await ethers.getContractFactory('Token');
+    const ddsToken = await Token.deploy('Dapp Dot Sol', 'DDS', 1000000);
+
+    await ddsToken.waitForDeployment();
+
+    const Crowdsale = await ethers.getContractFactory('Crowdsale');
+    const crowdsale = await Crowdsale.deploy(ddsToken.target);
+
+    await crowdsale.waitForDeployment();
 
     // Fixtures can return anything you consider useful for your tests
-    return { crowdsale, owner, addr1, addr2 };
+    return { crowdsale, ddsToken, owner, addr1, addr2 };
   }
 
   it('Deployment should assigne the correct name', async function () {
-    const { crowdsale, owner, addr1, addr2 } = await loadFixture(
-      deployCrowdsaleFixture
-    );
+    const { crowdsale } = await loadFixture(deployCrowdsaleFixture);
     expect(await crowdsale.name()).to.equal('Crowdsale');
+  });
+
+  it('Deployment should return the correct token address', async function () {
+    const { crowdsale, ddsToken } = await loadFixture(deployCrowdsaleFixture);
+    expect(await crowdsale.token()).to.equal(ddsToken.target);
   });
 });
