d4d27a628749e66e888ca61b65ce218d6531b276
dvauquelin <david.vauquelin@planisware.com>
2024-04-15 15:31:15 +0200

    Added WebApp Frontend

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="container mx-auto px-4">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
