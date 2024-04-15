48fc570320c5a390e0d27115e6b4f734ff2432ab
dvauquelin <david.vauquelin@planisware.com>
2024-04-15 16:11:12 +0200

    Added Loading account

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import Navigation from './components/Navigation';
+import { ethers } from 'ethers';
+import Info from './components/Info';
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    const account = ethers.getAddress(accounts[0]);
+    setAccount(account);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div className="container mx-auto px-4">
       <Navigation />
+      <hr />
+      {account && <Info account={account} />}
     </div>
   );
 }
